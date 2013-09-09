2e69ee6b8160e161f3f5195bd39b1f1dc78ea429
kcharvey <kcharvey@gmail.com>
2013-09-09 09:16:39 -0400

    all of the stuff that sets app1 apart (duh)

--- app1/urls.py
+++ app1/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -6,7 +7,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'app1.views.home', name='home'),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name="home")    
     # url(r'^app1/', include('app1.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
